5a43c7701e26a84511955e93d8cbbb515058b87a
isonic1 <justin.ison@gmail.com>
2018-10-29 11:50:12 -0400

    updated

--- ruby-jenkins.rb
+++ ruby-jenkins.rb
@@ -29,7 +29,7 @@ describe 'Testing Applitools' do
   it 'Applitools Test' do |e|
     @eyes.open(driver: @driver, app_name: "Branch Testing", test_name: e.full_description, viewport_size: {width: 1050, height: 750})
     @driver.get 'https://google.es'
-    @eyes.check_window 'Google Spain 12'
+    @eyes.check_window 'Google Spain 13'
     results = @eyes.close(false)
     #expect(results).not_to be_nil
     expect(results.passed?).to eq true
